8234e1be3b4c8e5a4a62dd58573ae3ee915b8f1b
konapeso <n2kgt1@gmail.com>
2019-07-27 13:51:02 +0900

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <TheNavbar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheNabvar from "@/components/TheNavbar";
+export default {
+  components: {
+    TheNavbar
+  }
+};
+</script>
+
+<style>
+.button--green {
+  display: inline-block;
+}
+</style>
---
+++ pages/post.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">What is Balut?</h1>
+      <p>If someone placed balut on your plate, you might think they were serving you a hardboiled egg.</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  line-height: 1.5;
+}
+
+article * {
+  margin-bottom: 1rem;
+}
+
+aside {
+  min-width: 280px;
+  max-width: 280px;
+  padding-left: 2rem;
+}
+
+.title {
+  font-size: 2rem;
+}
+</style>
